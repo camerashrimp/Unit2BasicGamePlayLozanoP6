98a597a2327b5d7e61cd55ff17f1df59ee3be6e4
camerashrimp <731406@student.dusd.net>
2026-03-19 13:27:26 -0700

    adding spawns

--- P6LozanoBasicGamePlay/Assets/Course Library/scripts/playercontroller.cs
+++ P6LozanoBasicGamePlay/Assets/Course Library/scripts/playercontroller.cs
@@ -4,6 +4,10 @@ public class playercontroller : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,9 +17,21 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            //Lanch a projectile from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
